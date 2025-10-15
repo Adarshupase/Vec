09447fe6273691e1773c7c86674585cb35838180
Adarsh Upase <138318435+Adarshupase@users.noreply.github.com>
2025-10-16 01:06:22 +0530

    Update slices.cpp

--- examples/slices.cpp
+++ examples/slices.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     Vec<Vec<int>> vec{{1,2,3},{4,5,6},{7,5,8},{9,10,11}};
-    Vec<Vec<int>> subvec = vec.sub(1,2);
+    Vec<Vec<int>> subvec = vec.slice(1,2);
     std::cout << subvec << '\n';
     
 
