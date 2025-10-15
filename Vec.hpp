305cad23de15f7880cd1871f29e8de418ed7f3e9
Adarsh Upase <138318435+Adarshupase@users.noreply.github.com>
2025-10-16 00:18:06 +0530

    cpp -> hpp

--- Vec.hpp
+++ Vec.hpp
@@ -1,3 +1,4 @@
+#pragma once 
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
@@ -255,64 +256,6 @@ class Vec {
         }
 };
 
-void dfs(int i,Vec<Vec<int>>& ans,Vec<int> &cur,Vec<int>& nums)
-{
-    if(i == nums.size())
-    {
-        ans.push_back(cur);
-        return ;
-    }
-    cur.push_back(nums[i]);
-    dfs(i+1,ans,cur,nums);
-    cur.pop_back();
-    dfs(i+1,ans,cur,nums);
-}
-
-
-
-Vec<Vec<int>> subsets(Vec<int>& nums)
-{
-    Vec<Vec<int>> ans;
-    Vec<int> cur;
-    dfs(0,ans,cur,nums);
-    return ans;
-
-
-}
-typedef struct {
-    std::string name;
-    unsigned int age;
-}Person;
-class Animal{
-    public:
-        std::string m_name;
-        unsigned int m_age;
-
-};
-int main()
-{
-    Person p{"Tom",45};
-    Vec<Person *> persons;
-    Animal dog{"Dog",12};
-    Vec<Animal> animals;
-    animals.push_back(dog);
-    persons.push_back(&p);
-    for(auto &p : persons)
-    {
-        std::cout << p->name << '\n';
-        std::cout << p->age << '\n';
-    }
-    for(auto &a : animals)
-    {
-        std::cout << a.m_name << '\n';
-        std::cout << a.m_age << '\n';
-    }
-    Vec<int> nums({1,2,3});
-    Vec<Vec<int>> ans = subsets(nums);
-    std::cout << "ans = " << ans << '\n';
-
-
-}
 /*
 variadic functions 
 https://stackoverflow.com/questions/1657883/variable-number-of-arguments-in-c
