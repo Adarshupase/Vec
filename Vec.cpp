6f55559559634588ee20af444bcc7fabdfcf57e9
Adarsh Upase <adarshupas2121@gmail.com>
2025-10-15 23:54:52 +0530

    Implemented Random access, support for nested types, negative indexing and slicing like python lists

--- Vec.cpp
+++ Vec.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 #define predict_size(x,y) ((x) + (y) + (y))
 
 
@@ -21,42 +22,98 @@ class Vec {
             m_size = 0;
         }
 
-        void printVec(void)
-        {
-            for(T* itr = m_container_start; itr != m_current_unused_ptr;++itr)
-            {
-                std::cout << *(itr) << '\n';
-            }
-        }
-
         Vec() : m_container_start(nullptr),
                 m_current_unused_ptr(nullptr),
                 m_container_end(nullptr),
                 m_size(0) {}
 
+        Vec(size_t size)
+            :m_container_start(nullptr),
+            m_current_unused_ptr(nullptr),
+            m_container_end(nullptr),
+            m_size(0)
+            {
+                reserve(size);
+
+            } 
+        
         Vec(std::initializer_list<T> list)
             : m_container_start(nullptr),
             m_current_unused_ptr(nullptr),
             m_container_end(nullptr),
             m_size(0)
+            {
+                size_t size_of_initializer = list.size();
+                reserve(size_of_initializer);
+                push_back(list);
+            }
+
+        
+        Vec(const Vec& other)
+            : m_container_start(nullptr),
+              m_current_unused_ptr(nullptr),
+              m_container_end(nullptr),
+              m_size(0)
         {
-            size_t size_of_initializer = list.size();
-            reserve(size_of_initializer);
-            push_back(list);
+            if (other.m_size > 0) {
+                reserve(other.capacity());
+                std::copy(other.m_container_start, other.m_container_start + other.m_size, m_container_start);
+                m_size = other.m_size;
+                m_current_unused_ptr = m_container_start + m_size;
+            }
+        }
+        Vec(Vec&& other) noexcept
+                : m_container_start(other.m_container_start),
+                m_current_unused_ptr(other.m_current_unused_ptr),
+                m_container_end(other.m_container_end),
+                m_size(other.m_size)
+        {
+            other.m_container_start = nullptr;
+            other.m_current_unused_ptr = nullptr;
+            other.m_container_end = nullptr;
+            other.m_size = 0;
+        }
+        Vec& operator=(const Vec& other)
+        {
+            if(this != &other)
+            {
+                reserve(other.capacity());
+                m_size = other.m_size;
+                std::copy(other.m_container_start,other.m_container_start + other.m_size,m_container_start);
+                m_current_unused_ptr = m_container_start + m_size;
+            }
+            return *this;
+        }
+        Vec& operator=(Vec&& other) noexcept {
+            if (this != &other) {
+                delete[] m_container_start;
+                m_container_start = other.m_container_start;
+                m_current_unused_ptr = other.m_current_unused_ptr;
+                m_container_end = other.m_container_end;
+                m_size = other.m_size;
+
+                other.m_container_start = nullptr;
+                other.m_current_unused_ptr = nullptr;
+                other.m_container_end = nullptr;
+                other.m_size = 0;
+            }
+            return *this;
+        }
+        void push_back(const T& element)
+        {
+            resize_if_neccessary(1);
+            *m_current_unused_ptr = element;
+            use(1);
         }
         void push_back(std::initializer_list<T> list)
         {
             int size_of_list = list.size();
             resize_if_neccessary(size_of_list);
-            if(m_current_unused_ptr + size_of_list > m_container_end)
-            {
-                resize(size_of_list);
-            }
             T* list_ptr = m_current_unused_ptr;
             
             for(const T &x :list)
             {
-                *(list_ptr++) = x;
+                *(list_ptr++) = x ;
             }
             use(size_of_list);
             
@@ -64,7 +121,7 @@ class Vec {
         }
         void pop_back()
         {
-            if(size > 0)
+            if(m_size > 0)
             {
                 unuse(1);
             }
@@ -75,10 +132,9 @@ class Vec {
         }
         void pop_back(size_t elements_to_remove)
         {
-            if(m_size - elements_to_remove >= 0)
+            if(elements_to_remove <= m_size)
             {
-                m_current_unused_ptr -= elements_to_remove;
-                m_size -= elements_to_remove;
+                unuse(elements_to_remove);
             }
             else
             {
@@ -86,16 +142,68 @@ class Vec {
                 exit(1);
             }
         }
-        
+        T& operator[](int index)
+        {
+            if(index < 0){
+                index = m_size + index;
+
+            }
+            if(index < 0 || static_cast<size_t>(index) >= m_size){
+                 printf("Index out of range\n");
+                 exit(1);
+            }
+
+            return *(m_container_start + index);
+        }
+        Vec<T> sub(size_t start,size_t end)
+        {
+            if(start > end || start < 0 || static_cast<size_t>(end) > m_size)
+            {
+                printf("Invalid slice range [%zu:%zu]\n", start, end);
+                exit(1);
+            }
+            Vec<T> slice ;
+            slice.reserve(end - start);
+            for(int i = start ; i < end ; ++i)
+            {
+                slice.push_back((*this)[i]);
+            }
+            return slice;
+            
+        }
+
         
 
 
-        size_t size()
+        size_t size() const
         {
             return m_size;
         }
+        size_t capacity() const
+        {
+            return m_container_end - m_container_start;
+        }
+        // overload std::cout to print Vec<T>,Vec<Vec<T>> etc... 
+        friend std::ostream& operator<<(std::ostream& os , const Vec<T>& vec)
+        {
+            os << "[";
+            for(size_t i = 0; i < vec.m_size; ++i)
+            {
+                os << vec.m_container_start[i];
+                if(i != vec.m_size-1)
+                    os << ",";
+
+            }
+            os << "]";
+            return os;
+        }
+        T* begin() {return m_container_start;}
+        T* end() {return m_container_start + m_size;};
+        T* begin() const {return m_container_start;}
+        T* end() const {return m_container_start + m_size;}
         ~Vec() {
-        delete[] m_container_start;
+            if (m_container_start)
+                delete[] m_container_start;
         }
 
 
@@ -113,15 +221,6 @@ class Vec {
         void resize(size_t size_of_list)
         {
             size_t resize_length = predict_size(m_size,size_of_list);
-
-            // m_container_start_temp = new T(resize_length);
-
-            // std::copy(m_container_start,m_container_end,m_current_unused_ptr,m_container_start_temp);
-
-            // delete m_container_start;
-            // m_container_start = m_container_temp;
-            // m_current_unused_ptr = m_container_start + size;
-            // m_container_end = m_container_start + resize_length;
             reserve(resize_length);
 
         }
@@ -157,14 +256,37 @@ class Vec {
         }
 };
 
+void dfs(int i,Vec<Vec<int>>& ans,Vec<int> &cur,Vec<int>& nums)
+{
+    if(i == nums.size())
+    {
+        ans.push_back(cur);
+        return ;
+    }
+    cur.push_back(nums[i]);
+    dfs(i+1,ans,cur,nums);
+    cur.pop_back();
+    dfs(i+1,ans,cur,nums);
+}
+
+
+
+Vec<Vec<int>> TwoSum(Vec<int>& nums)
+{
+    Vec<Vec<int>> ans;
+    Vec<int> cur;
+    dfs(0,ans,cur,nums);
+    return ans;
+
+
+}
+
+
 int main()
 {
-    Vec<int> myVec{{1,2,34}};
-    myVec.push_back({3,4,5,6,54,2});
-    myVec.printVec();
-    myVec.collapse();
-    myVec.size();
-    myVec.printVec();
+    Vec<int> nums({1,2,3});
+    Vec<Vec<int>> ans = TwoSum(nums);
+    std::cout << "ans = " << ans << '\n';
 
 
 }
