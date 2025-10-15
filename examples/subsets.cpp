05f9783718fc3930c3c2ddf1df5b9c0280ad7efb
Adarsh Upase <138318435+Adarshupase@users.noreply.github.com>
2025-10-16 00:37:32 +0530

    remove commented code
    Removed commented code

--- examples/subsets.cpp
+++ examples/subsets.cpp
@@ -36,30 +36,3 @@ int main()
 
 }
 
-
-// typedef struct {
-//     std::string name;
-//     unsigned int age;
-// }Person;
-// class Animal{
-//     public:
-//         std::string m_name;
-//         unsigned int m_age;
-
-// };
-//  Person p{"Tom",45};
-//     Vec<Person *> persons;
-//     Animal dog{"Dog",12};
-//     Vec<Animal> animals;
-//     animals.push_back(dog);
-//     persons.push_back(&p);
-//     for(auto &p : persons)
-//     {
-//         std::cout << p->name << '\n';
-//         std::cout << p->age << '\n';
-//     }
-//     for(auto &a : animals)
-//     {
-//         std::cout << a.m_name << '\n';
-//         std::cout << a.m_age << '\n';
-//     }
